66e562133557549d8e782a2df2b8f37c70564ac0
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-23 16:56:34 +0545

    component added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rajan Devkota",
+    author_email="r.devkota.98@gmail.com",
+    packages=find_packages()
+)
